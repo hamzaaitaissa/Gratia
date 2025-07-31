1f2d8e028594e26cbb470aaffa49e15721eefb5b
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-07-31 22:45:50 +0100

    Dev: We so back and moving steady

---
+++ Gratia.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Gratia.Domain.Entities;
+using Gratia.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gratia.Infrastructure.Repositories
+{
+    internal class UserRepository : IUserRepository
+    {
+        public Task<User> AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
