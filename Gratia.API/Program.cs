e49f4c2efe7917986bcdde89cb9ae7de0191c97a
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-08-13 23:32:00 +0100

    Dev: adding auth scheme

--- Gratia.API/Program.cs
+++ Gratia.API/Program.cs
@@ -3,7 +3,10 @@ using Gratia.Application.Services;
 using Gratia.Domain.Repositories;
 using Gratia.Infrastructure.Data;
 using Gratia.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,25 @@ builder.Services.AddDbContext<GratiaDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//addin JWt Config
+var jwtKey = builder.Configuration.GetValue<string>("AppSettings:Token");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 
 
 // Add services to the container.
@@ -33,6 +55,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
