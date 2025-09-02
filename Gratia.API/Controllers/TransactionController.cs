15a803fff6dd064cfbb95aba74523583eef0a866
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-09-02 21:01:40 +0100

    dev: transaction controller

---
+++ Gratia.API/Controllers/TransactionController.cs
@@ -0,0 +1,20 @@
+﻿using Gratia.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gratia.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+
+    }
+}
